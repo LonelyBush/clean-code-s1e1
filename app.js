bef18fd48d5bf4cb685fb2f2c2ffe6eb7a483ab2
LonelyBush <lonely.bush68@gmail.com>
2022-05-30 20:22:08 +0300

    refactor: add mix button-style class to buttons acc 2.1 rule from html-and-css-extended.md

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button-style edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button-style delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
